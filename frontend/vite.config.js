e29b74b96f92538a7f3ede76ef8dfd3f344bb3d5
Shikhar Srivastav <shikharsrivastav9450@gmail.com>
2025-03-25 01:27:22 +0530

    vercel setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
    proxy: {
     '/api': {
-      target: 'http://localhost:4000',
+      // target: 'http://localhost:4000',
+      target: "https://aditya-rise-institute-git-main-arcis-projects-2e5ddd3b.vercel.app/",
       changeOrigin: true 
       },
    }
